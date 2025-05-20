63c8deb9426d107a67ab28b530c0b639d6ca68a2
Justus <justus.d2025@gmail.com>
2025-05-21 00:34:01 +0200

    Create move_to.py
    Some testing

---
+++ move_to.py
@@ -0,0 +1,30 @@
+from math import sin, cos,asin,sqrt
+def move_to(x1,y1,x2,y2):
+    a = x2 - x1
+    b = y2 - y1
+    
+    alpha = asin(a / sqrt(a ** 2 + b ** 2))
+    dx = sin(alpha)
+    dy = cos(alpha)
+    x1 += dx
+    y1 += dy
+    
+    return [x1,y1]
+def same_pos(p1,p2):
+    return get_as_int(p1) == get_as_int(p2)
+def get_as_int(p):
+    return [int(i) for i in p]
+pos1 = [2,1]
+pos2 = [12,9]
+while not same_pos(pos1,pos2):
+    print('------------------------')
+    for y in range(10):
+        for x in range(15):
+            if [x,y] == get_as_int(pos1) or [x,y] == get_as_int(pos2):
+                print('#',end='')
+            else:
+                print(' ',end='')
+        print()
+    pos1 = move_to(*pos1,*pos2)
+    
+print('finished')
